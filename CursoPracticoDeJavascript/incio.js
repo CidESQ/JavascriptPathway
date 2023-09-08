7762080a02bf6a1536fd8ab384db02df35f09d23
cidesgo@gmail.com <cidesgo@gmail.com>
2023-09-07 23:17:06 -0600

    Agregamos la primera clase del curso practico de JS corregido

--- CursoPracticoDeJavascript/incio.js
+++ CursoPracticoDeJavascript/incio.js
@@ -14,5 +14,41 @@ function persona(name, lastname, nickname) {
   this.nickname = nickname;
   let completeName = name + lastname;
 
-  console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + nickname + ".")
+  console.log(
+    "Mi nombre es " +
+      completeName +
+      ", pero prefiero que me digas " +
+      nickname +
+      "."
+  );
+}
+
+// const tipoDeSuscripcion = "Basic";
+
+// switch (tipoDeSuscripcion) {
+//    case "Free":
+//        console.log("Solo puedes tomar los cursos gratis");
+//        break;
+//    case "Basic":
+//        console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+//        break;
+//    case "Expert":
+//        console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+//        break;
+//    case "ExpertPlus":
+//        console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+//        break;
+// }
+
+const tipoDeSuscripcion = "Basic";
+if (tipoDeSuscripcion === "Free") {
+  console.log("Solo puedes tomar los cursos gratis");
+} else if (tipoDeSuscripcion === "Basic") {
+  console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+} else if (tipoDeSuscripcion === "Expert") {
+  console.log("Puedes tomar casi todos los cursos de Platzi durante un año.");
+} else if (tipoDeSuscripcion) {
+  console.log(
+    "Tu y alguien mas pueden tomar TODOS los cursos de Platzi durante un año."
+  );
 }
