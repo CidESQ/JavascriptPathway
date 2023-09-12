f5fb7d5c4e592834ec1fb3ba619059f78266a876
cidesgo@gmail.com <cidesgo@gmail.com>
2023-09-12 09:14:49 -0600

    Actualizacion de los archivos html y js

--- CursoPracticoDeJavascript/script.js
+++ CursoPracticoDeJavascript/script.js
@@ -1,1 +1,14 @@
-console.log("Hola mundo.")
+const tituloH1 = document.querySelector("h1");
+const primerParrafo = document.querySelector("p");
+const segundoParrafo = document.querySelector(".segundoParrafo");
+const tercerParrafo = document.querySelector("#tercerParrafo");
+const input = document.querySelector("input");
+
+// console.log({
+//   tituloH1,
+//   primerParrafo,
+//   segundoParrafo,
+//   tercerParrafo,
+//   input,
+// });
+console.log(input.value)
